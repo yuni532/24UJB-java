cc32ca3e30029ae62f359774999cda8201457c49
21-manic-005g <21-manic-005g@21-manic-005>
2024-03-21 14:34:07 +0900

    단항 연산자

---
+++ Java projects/src/d0321/Operator2Example.java
@@ -0,0 +1,24 @@
+package d0321;
+
+public class Operator2Example {
+
+	public static void main(String[] args) {
+		int x = 3;
+		int y = -4;
+		int z = +5;
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		
+		int result1 = +y;
+		int result2 = y;
+		int result3 = -y;
+		
+		System.out.println(result1);
+		System.out.println(result2);
+		System.out.println(result3);
+
+	}
+
+}
