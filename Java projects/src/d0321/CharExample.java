71f4a7a6b820c6ce8a22a735dffdb673dbf89f3c
21-manic-005g <21-manic-005g@21-manic-005>
2024-03-21 11:52:25 +0900

    아스키 코드 대문자 A는 65 소문자 a는  97

---
+++ Java projects/src/d0321/CharExample.java
@@ -0,0 +1,14 @@
+package d0321;
+
+public class CharExample {
+
+	public static void main(String[] args) {
+		char c1 = 'A';
+		char c2 = 65;
+		
+		System.out.println(c1);
+		System.out.println(c2);
+
+	}
+// 대문자 A는 65 소문자 a는 97
+}
