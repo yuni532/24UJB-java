15fe9a1d00c47498b8a1af024f939e37ee6c0a3b
21-manic-005g <21-manic-005g@21-manic-005>
2024-03-21 16:32:09 +0900

    연산자

---
+++ Java projects/src/d0321/Operatour6Example.java
@@ -0,0 +1,21 @@
+package d0321;
+
+public class Operatour6Example {
+	public static void main(String[] args) {
+	byte a = 10;
+	int b = 5;
+	int c = 7;
+	double d = 4.8;
+	
+	int result1 = a + b;
+	double result2 = c + d;
+	
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+	System.out.println(d);
+	
+	System.out.println(result1);
+	System.out.println(result2);
+	}
+}
