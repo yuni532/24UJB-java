be017f3f892ccfd716577c7e4792d1cb1f990061
21-manic-005g <21-manic-005g@21-manic-005>
2024-03-21 17:43:39 +0900

    scanner 사용

---
+++ Java projects/src/d0321/test1.java
@@ -0,0 +1,20 @@
+package d0321;
+
+
+import java.util.Scanner;
+
+public class test1{
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		int a = sc.nextInt();
+		
+	
+		sc.close();
+		System.out.print("height = ");
+
+		System.out.println("Your height is " + a + "cm.");
+		
+	}
+
+}
