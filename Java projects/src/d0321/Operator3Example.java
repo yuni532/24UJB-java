b167718753d9833226cb36a00e2753d44e083f62
21-manic-005g <21-manic-005g@21-manic-005>
2024-03-21 14:41:26 +0900

    연산자

---
+++ Java projects/src/d0321/Operator3Example.java
@@ -0,0 +1,12 @@
+package d0321;
+
+public class Operator3Example {
+	public static void main(String[] args) {
+		short s = 100;
+		// short result = -s; 
+		int result2 =  -s;
+		
+		System.out.println(s);
+		System.out.println(result2);
+	}
+}
