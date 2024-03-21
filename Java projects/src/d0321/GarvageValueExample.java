c2e6f8c14385376f2973da3faf2d7a1db6eb4e25
21-manic-005g <21-manic-005g@21-manic-005>
2024-03-21 11:47:19 +0900

    GarvageValue 예

---
+++ Java projects/src/d0321/GarvageValueExample.java
@@ -0,0 +1,16 @@
+package d0321;
+
+public class GarvageValueExample {
+
+	public static void main(String[] args) {
+		byte var1 = 125;
+		byte var2 = 125;
+		
+		for(int i=0;i<5;i++) {
+			var1++;
+			var2++;
+			System.out.println("var1: " + var1 + "\t" + "var2" + var2);
+		}
+	}
+
+}
