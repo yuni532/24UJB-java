0972842d36a97b569e55210c4d1dcc6cd7eabbf3
21-manic-005g <21-manic-005g@21-manic-005>
2024-03-22 15:02:33 +0900

    String concat Example

---
+++ Java projects/src/d0322/StringConcatExample.java
@@ -0,0 +1,25 @@
+
+package d0322;
+
+public class StringConcatExample {
+	public static void maing(String [] args) {
+			String str1 = "Java";
+			String str2 = "JDK";
+			
+			String str3 = str1 + str2;
+			String str4 = str1 + " " + str2;
+			
+			String str5 = "JDK" + 3 + 3.0;
+			String str6 = 3 + 3.0 + "JDK";
+			
+			System.out.println(str1);
+			System.out.println(str2);
+			System.out.println(str3);
+			System.out.println(str4);
+			System.out.println(str5);
+			System.out.println(str6);
+			
+			
+			
+	}
+}
